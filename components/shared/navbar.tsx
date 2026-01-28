c880ecfe052b673de3aa0fdbd7216caa8576abb6
jsa498 <jaskaran1431085@gmail.com>
2026-01-28 18:05:02 +0530

    refactor: Update "Resources" navigation link to "Our Work".

--- components/shared/navbar.tsx
+++ components/shared/navbar.tsx
@@ -11,7 +11,7 @@ import { cn } from "@/lib/utils";
 const links = [
   { href: "/", label: "Home" },
   { href: "/services", label: "Services" },
-  { href: "/resources", label: "Resources" },
+  { href: "/our-work", label: "Our Work" },
   { href: "/#contact", label: "Contact" },
 ];
 
